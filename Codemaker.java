2afa16299a682c8d5fc0a6743ff5c41120967cb1
Arnav Parashar <Arnav@Arnavs-Air.nicholsschool.org>
2018-05-09 15:18:44 -0400

    Codemaker class

--- Codemaker.java
+++ Codemaker.java
@@ -9,29 +9,64 @@ public class Codemaker
 { 
   private Code _code;
   private Scanner _sc;
+  private final char[] _colors = {'R', 'O', 'Y', 'G', 'B', 'P', 'W', 'T'};
+  private final int CODE_LENGTH = 5;
+  /**
+   * Used when computer is the codemaker, this creates a new Codemaker object and makes a random code. 
+   * 
+   * @param sc Scanner a pointer for the _sc so that Codemaker can prompt the user when necessary
+   * @author Arnav Parashar
+   */
   
   public Codemaker(Scanner sc) 
   {
       _sc = sc;
+      String cpuCode  = "";
+      for(int i = 0; i < CODE_LENGTH; i++)
+          cpuCode = cpuCode + _colors[(int)(Math.random() * _colors.length)];
+      
+      _code = new Code(cpuCode);
+      System.out.println(cpuCode);
   }
   
+    /**
+   * Used when the user is the codemaker, this creates a new Codemaker object and takes in the user's code. 
+   * 
+   * @param input String the code the user would like to use
+   * @param sc Scanner a pointer for the _sc so that Codemaker can prompt the user when necessary
+   * @author Arnav Parashar
+   */
   public Codemaker(String input, Scanner sc)
   {
       _sc = sc;
+      _code = new Code(input);
   }
   
-  public Code getCode()
+  /**
+   * Used when the CPU is the codemaker, this responds with the correct amount of black and white pegs for the user's guess
+   * 
+   * @param userGuess String the guess of the user to be compared to the correct code
+   * @return a String with the correct amount of black and white pegs for the guess
+   * @author Arnav Parashar
+   */
+  
+  public String getCPUResponse(String userGuess) 
   {
-    return null;
+      return _code.getResponse(userGuess);
   }
   
-  public String getCPUResponse(String guess) 
+    /**
+   * Used when the user is the codemaker, has the user respond with what they think to be the correct amount of black and white pegs for the CPU's guess
+   * 
+   * @param cpuGuess String the guess of the cpu to be compared to the correct code
+   * @return a String with the user's amount of black and white pegs for the guess
+   * @author Arnav Parashar
+   */
+  public String getUserResponse(String cpuGuess)
   {
-      return null;
-  }
-  
-  public String getUserResponse(String guess)
-  {
-      return null;
+      System.out.println("The computer responded with " + cpuGuess + "\n Now you can tell what it did wrong. \n (Ex. bb w)" );
+      String response = _sc.nextLine();
+      
+      return response;
   } 
 }
